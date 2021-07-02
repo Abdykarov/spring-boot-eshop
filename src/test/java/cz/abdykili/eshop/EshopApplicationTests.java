515211f57711b101a5cf44d26340d2eddaa826f0
Ilias Abdykarov <ilias.abdykarov@gmail.com>
2021-07-02 09:48:54 +0200

    spring init files added

---
+++ src/test/java/cz/abdykili/eshop/EshopApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.abdykili.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
