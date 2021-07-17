c65e72f93cd85641045b942fc80ab02b07c64c25
Ilias Abdykarov <ilias.abdykarov@gmail.com>
2021-07-17 19:42:29 +0200

    lombok added

--- src/main/java/cz/abdykili/eshop/EshopApplication.java
+++ src/main/java/cz/abdykili/eshop/EshopApplication.java
@@ -3,11 +3,20 @@ package cz.abdykili.eshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class  EshopApplication {
 
+	public static final Logger logger = Logger.getLogger(EshopApplication.class.getName());
+
 	public static void main(String[] args) {
 		SpringApplication.run(EshopApplication.class, args);
+
 	}
 
 }
--- src/main/java/cz/abdykili/eshop/model/ProductDto.java
+++ src/main/java/cz/abdykili/eshop/model/ProductDto.java
@@ -1,5 +1,8 @@
 package cz.abdykili.eshop.model;
 
+import lombok.Data;
+
+@Data
 public class ProductDto {
 
     private Long id;
@@ -9,51 +12,5 @@ public class ProductDto {
     private Long stock;
     private String image;
 
-    public Long getId() {
-        return id;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Long getPrice() {
-        return price;
-    }
-
-    public void setPrice(Long price) {
-        this.price = price;
-    }
-
-    public Long getStock() {
-        return stock;
-    }
-
-    public void setStock(Long stock) {
-        this.stock = stock;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
