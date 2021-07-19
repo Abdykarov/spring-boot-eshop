5f6082ea99256b2218b047ae78c90bc028a55bb9
Ilias Abdykarov <ilias.abdykarov@gmail.com>
2021-07-19 16:21:01 +0200

    ITA - final changes v1.2

--- src/main/java/cz/abdykili/eshop/repository/ProductRepository.java
+++ src/main/java/cz/abdykili/eshop/repository/ProductRepository.java
@@ -3,8 +3,5 @@ package cz.abdykili.eshop.repository;
 import cz.abdykili.eshop.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ProductRepository extends JpaRepository<Product, Long> {
-//    Optional<Product> findProductById(Long id);
 }
