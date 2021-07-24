8620247106eb1e5049ba6e7812073f92998b57a4
Ilias Abdykarov <ilias.abdykarov@gmail.com>
2021-07-24 16:45:17 +0200

    ita - validate endpoints - finished

---
+++ src/main/java/cz/abdykili/eshop/exception/ProductNotFoundException.java
@@ -0,0 +1,19 @@
+package cz.abdykili.eshop.exception;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Setter
+@Getter
+public class ProductNotFoundException extends ItaRequestException{
+    private final String message;
+    private final HttpStatus httpStatus;
+
+    public ProductNotFoundException(String message, HttpStatus httpStatus){
+        super(message, httpStatus);
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+}
--- src/main/java/cz/abdykili/eshop/service/impl/ProductServiceImpl.java
+++ src/main/java/cz/abdykili/eshop/service/impl/ProductServiceImpl.java
@@ -1,6 +1,9 @@
 package cz.abdykili.eshop.service.impl;
 
 import cz.abdykili.eshop.domain.Product;
+import cz.abdykili.eshop.exception.ItaRequestException;
+import cz.abdykili.eshop.exception.ProductNotFoundException;
+import cz.abdykili.eshop.exception.ValidationException;
 import cz.abdykili.eshop.model.ProductResponseDto;
 import cz.abdykili.eshop.model.ProductRequestDto;
 import cz.abdykili.eshop.repository.ProductRepository;
@@ -9,9 +12,11 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import java.math.BigDecimal;
 import java.util.List;
 
 import java.util.stream.Collectors;
@@ -45,7 +50,8 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public ProductResponseDto findProduct(Long id){
         final Product product = productRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Product with id " + id + " not found!"));
+                .orElseThrow(() -> new ProductNotFoundException("Product with id " + id + " not found!",
+                        HttpStatus.NOT_FOUND));
         ProductResponseDto responseProduct = mapToResponse(product);
 
         logger.info("Returning the product with id {} from repository", product.getId());
@@ -58,33 +64,39 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public ProductResponseDto saveProduct(ProductRequestDto productRequestDto){
-        Product newProduct = mapToEntity(productRequestDto);
-        final Product savedProduct = productRepository.save(newProduct);
-        ProductResponseDto productResponseDto = mapToResponse(savedProduct);
+        if(validateRequestDto(productRequestDto)){
+            Product newProduct = mapToEntity(productRequestDto);
+            final Product savedProduct = productRepository.save(newProduct);
+            ProductResponseDto productResponseDto = mapToResponse(savedProduct);
 
-        logger.info("Saving the product with id {}", savedProduct.getId());
-        if(logger.isDebugEnabled()){
-            logger.debug("Incoming payload {}, outgoing payload {} ", productRequestDto, productResponseDto);
+            logger.info("Saving the product with id {}", savedProduct.getId());
+            if(logger.isDebugEnabled()){
+                logger.debug("Incoming payload {}, outgoing payload {} ", productRequestDto, productResponseDto);
+            }
+
+            return productResponseDto;
         }
-
-        return productResponseDto;
+        return null;
     }
 
     @Override
     public ProductResponseDto updateProduct(ProductRequestDto productRequestDto, Long id){
-        final Product product = productRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Product with id " + id + " not found!"));
-        Product productToSave = mapToEntity(productRequestDto)
-                .setId(product.getId());
-        final Product updatedProduct = productRepository.save(productToSave);
-        ProductResponseDto productResponseDto = mapToResponse(updatedProduct);
+        if(validateRequestDto(productRequestDto)){
+            final Product product = productRepository.findById(id)
+                    .orElseThrow(() -> new EntityNotFoundException("Product with id " + id + " not found!"));
+            Product productToSave = mapToEntity(productRequestDto)
+                    .setId(product.getId());
+            final Product updatedProduct = productRepository.save(productToSave);
+            ProductResponseDto productResponseDto = mapToResponse(updatedProduct);
 
-        logger.info("Updating the product with id {}", product.getId());
-        if(logger.isDebugEnabled()){
-            logger.debug("Incoming payload {}, outgoing payload {} ", productRequestDto, productResponseDto);
+            logger.info("Updating the product with id {}", product.getId());
+            if(logger.isDebugEnabled()){
+                logger.debug("Incoming payload {}, outgoing payload {} ", productRequestDto, productResponseDto);
+            }
+
+            return productResponseDto;
         }
-
-        return productResponseDto;
+        return null;
     }
 
     @Override
@@ -114,6 +126,37 @@ public class ProductServiceImpl implements ProductService {
                 .setPrice(savedProduct.getPrice())
                 .setStock(savedProduct.getStock())
                 .setName(savedProduct.getName());
+    }
+
+    private boolean validateRequestDto(ProductRequestDto productRequestDto){
+        if(productRequestDto.getName() == ""){
+            throw new ValidationException("Name is empty", HttpStatus.CONFLICT);
+        }
+        if(productRequestDto.getName().length() > 256){
+            throw new ValidationException("Maximum names length is 256 ch, current is " +
+                    productRequestDto.getName().length(), HttpStatus.CONFLICT);
+        }
+        if(productRequestDto.getDescription() == ""){
+            throw new ValidationException("Description is empty", HttpStatus.CONFLICT);
+        }
+        if(productRequestDto.getDescription().length() > 256){
+            throw new ValidationException("Maximum descriptions length is 512 ch, current is " +
+                    productRequestDto.getDescription().length(), HttpStatus.CONFLICT);
+        }
+        if(!(productRequestDto.getPrice().compareTo(BigDecimal.ZERO) > 0) ||
+                productRequestDto.getPrice() == null
+        ){
+            throw new ValidationException("Price must be higher than 0 and mustn't be null",
+                    HttpStatus.CONFLICT);
+        }
+        if(productRequestDto.getStock() < 0){
+            throw new ValidationException("Stock must be positive, current is " +
+                    productRequestDto.getStock(), HttpStatus.CONFLICT);
+        }
+        if(productRequestDto.getImage() == ""){
+            throw new ValidationException("Image is empty", HttpStatus.CONFLICT);
+        }
+        return true;
     }
 
 }
