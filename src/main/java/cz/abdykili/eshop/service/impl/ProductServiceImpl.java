ab4c372b36f4a00e40f519849622d31073d4000c
Ilias Abdykarov <ilias.abdykarov@gmail.com>
2021-07-24 13:35:33 +0200

    ITA - Set up logback - finished

--- src/main/java/cz/abdykili/eshop/service/impl/ProductServiceImpl.java
+++ src/main/java/cz/abdykili/eshop/service/impl/ProductServiceImpl.java
@@ -6,6 +6,8 @@ import cz.abdykili.eshop.model.ProductRequestDto;
 import cz.abdykili.eshop.repository.ProductRepository;
 import cz.abdykili.eshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,30 +22,52 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
-
+    private final Logger logger = LoggerFactory.getLogger(ProductServiceImpl.class);
     @Autowired
     private final ProductRepository productRepository;
 
     @Override
     public List<ProductResponseDto> findAll(){
-        return productRepository.findAll().stream()
+        List<ProductResponseDto> products = productRepository.findAll().stream()
                 .map(p -> mapToResponse(p))
                 .collect(Collectors.toList());
+
+        logger.info("Returning {} products from repository", products.size());
+        if(logger.isDebugEnabled()){
+            for(ProductResponseDto product : products){
+                logger.debug("Returning the product {}", product);
+            }
+        }
+
+        return products;
     }
 
     @Override
     public ProductResponseDto findProduct(Long id){
         final Product product = productRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Product with id " + id + " not found!"));
+        ProductResponseDto responseProduct = mapToResponse(product);
 
-        return mapToResponse(product);
+        logger.info("Returning the product with id {} from repository", product.getId());
+        if(logger.isDebugEnabled()){
+            logger.debug("Returning the product {}", responseProduct);
+        }
+
+        return responseProduct;
     }
 
     @Override
     public ProductResponseDto saveProduct(ProductRequestDto productRequestDto){
         Product newProduct = mapToEntity(productRequestDto);
         final Product savedProduct = productRepository.save(newProduct);
-        return mapToResponse(savedProduct);
+        ProductResponseDto productResponseDto = mapToResponse(savedProduct);
+
+        logger.info("Saving the product with id {}", savedProduct.getId());
+        if(logger.isDebugEnabled()){
+            logger.debug("Incoming payload {}, outgoing payload {} ", productRequestDto, productResponseDto);
+        }
+
+        return productResponseDto;
     }
 
     @Override
@@ -53,14 +77,23 @@ public class ProductServiceImpl implements ProductService {
         Product productToSave = mapToEntity(productRequestDto)
                 .setId(product.getId());
         final Product updatedProduct = productRepository.save(productToSave);
+        ProductResponseDto productResponseDto = mapToResponse(updatedProduct);
 
-        return mapToResponse(updatedProduct);
+        logger.info("Updating the product with id {}", product.getId());
+        if(logger.isDebugEnabled()){
+            logger.debug("Incoming payload {}, outgoing payload {} ", productRequestDto, productResponseDto);
+        }
+
+        return productResponseDto;
     }
 
     @Override
     public void deleteProduct(Long id){
         final Product product = productRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Product with id " + id + " not found!"));
+
+        logger.info("Deleting the product with id {}", product.getId());
+
         productRepository.deleteById(id);
     }
 
